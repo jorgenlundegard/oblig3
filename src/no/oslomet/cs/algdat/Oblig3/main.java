40b951683f91e30f939fbbf49b7b133cfab84940
madsa101 <lundbye.mads@yahoo.no>
2019-11-02 11:47:30 +0100

    Oppgave 0 og 1

--- src/no/oslomet/cs/algdat/Oblig3/main.java
+++ src/no/oslomet/cs/algdat/Oblig3/main.java
@@ -1,11 +1,26 @@
 package no.oslomet.cs.algdat.Oblig3;
 
+import java.util.Comparator;
+
 public class main {
     //klasse for testing etc. TODO: slett før levering
     public static void main(String[] args){
         System.out.println("Hei");
 
+        //oppg. 0:
+        ObligSBinTre<Character> charTre = new ObligSBinTre<>(Comparator.naturalOrder());
+        System.out.println(charTre.antall());
 
+        ObligSBinTre<Character> intTre = new ObligSBinTre<>(Comparator.naturalOrder());
+        System.out.println(charTre.antall());
 
+        ObligSBinTre<Character> stringTre = new ObligSBinTre<>(Comparator.naturalOrder());
+        System.out.println(charTre.antall());
+
+        //oppg. 1:
+        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        for(int verdi: a){tre.leggInn(verdi);}
+        System.out.println(tre.antall());
     }
 }
