7a39fa855aed72f24b072eeece7a655467590f5d
madsa101 <lundbye.mads@yahoo.no>
2019-11-02 11:04:35 +0100

    la alt i package no.oslomet.cs.algdat.Oblig3

---
+++ src/no/oslomet/cs/algdat/Oblig3/main.java
@@ -0,0 +1,8 @@
+package no.oslomet.cs.algdat.Oblig3;
+
+public class main {
+    //klasse for testing etc. TODO: slett før levering
+    public static void main(String[] args){
+        System.out.println("Hei");
+    }
+}
